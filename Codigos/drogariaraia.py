6fd658f019b39ee09818087a79ef0b54e8947e9e
danguerguem <danielguerguem12@gmail.com>
2025-02-23 12:49:24 -0300

    Adicionando pasta ao repositório

--- Codigos/drogariaraia.py
+++ Codigos/drogariaraia.py
@@ -22,7 +22,7 @@ def salvar_produtos_dinamicamente(produtos_pagina):
 
 def run():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         base_url = "https://www.drogaraia.com.br/medicamentos/remedios.html"
